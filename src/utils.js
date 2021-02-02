dde6ef28e26750bf5e392cb1569daf3e387fc37c
arielvergarautn <arie@est.frba.utn.edu.ar>
2021-02-02 06:11:19 -0300

    new url slack

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,6 @@ export const sendMessageToSlack = (message) => {
         const requestOptions = {
             method: 'POST',
             body: JSON.stringify({ "text": message }),
-            mode: 'no-cors'
         };
         fetch(process.env.REACT_APP_SLACK_URL, requestOptions);
     } catch (e) {
