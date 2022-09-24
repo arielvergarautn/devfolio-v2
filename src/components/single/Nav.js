5be275ad1293fc986efd32f35a5829739e24b826
Ariel Vergara <avergara@intercept.cloud>
2022-09-24 20:43:51 +0200

    Creating a sidebar for mobile

--- src/components/single/Nav.js
+++ src/components/single/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 //Router
 import {
     Link,
@@ -7,64 +7,69 @@ import {
 //Animations
 import { motion } from 'framer-motion'
 import { lineNavAnim } from '../../animation'
+import SidebarMobile from './SidebarMobile'
 
 const Nav = () => {
 
     const { pathname } = useLocation();
+    const [sidebarActive, setSidebarActive] = useState(false);
 
     return (
-        <nav>
-            <h1>
-                <Link id='logo' to="/">Devfolio</Link>
-            </h1>
-            <ul>
-                <li>
-                    <div class="link-container">
-                        <Link to="/home">Home</Link>
-                        {
-                            (pathname === '/' || pathname === '/home') && (
-                                <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
-                            )
-                        }
-                    </div>
-                </li>
-                <li>
-                    <div class="link-container">
-                        <Link to="/about">About me</Link>
-                        {
-                            pathname === '/about' && (
-                                <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
-                            )
-                        }
-                    </div>
-                </li>
-                <li>
-                    <div class="link-container">
-                        <Link to="/work">My projects</Link>
-                        {
-                            pathname === '/work' && (
-                                <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
-                            )
-                        }
-                    </div>
-                </li>
-                <li>
-                    <div class="link-container">
-                        <Link to="/contact">Contact</Link>
-                        {
-                            pathname === '/contact' && (
-                                <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
-                            )
-                        }
-                    </div>
-                </li>
-                <li className='menu'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className='header'>
+                <h1>
+                    <Link className='logo' to="/">Devfolio</Link>
+                </h1>
+                <ul>
+                    <li>
+                        <div className="link-container">
+                            <Link to="/home">Home</Link>
+                            {
+                                (pathname === '/' || pathname === '/home') && (
+                                    <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
+                                )
+                            }
+                        </div>
+                    </li>
+                    <li>
+                        <div className="link-container">
+                            <Link to="/about">About me</Link>
+                            {
+                                pathname === '/about' && (
+                                    <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
+                                )
+                            }
+                        </div>
+                    </li>
+                    <li>
+                        <div className="link-container">
+                            <Link to="/work">My projects</Link>
+                            {
+                                pathname === '/work' && (
+                                    <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
+                                )
+                            }
+                        </div>
+                    </li>
+                    <li>
+                        <div className="link-container">
+                            <Link to="/contact">Contact</Link>
+                            {
+                                pathname === '/contact' && (
+                                    <motion.div variants={lineNavAnim} initial='hidden' animate='show' className="line"></motion.div>
+                                )
+                            }
+                        </div>
+                    </li>
+                    <li className='menu' onClick={() => setSidebarActive(true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                    </li>
+                </ul>
+            </nav>
+            <SidebarMobile sidebarActive={sidebarActive} setSidebarActive={setSidebarActive} />
+        </>
     )
 }
 
--- src/components/single/SidebarMobile.js
+++ src/components/single/SidebarMobile.js
@@ -1,6 +1,43 @@
-function SidebarMobile() {
+//Router
+import {
+  Link,
+} from 'react-router-dom'
+
+function SidebarMobile({sidebarActive, setSidebarActive}) {
+
+  const handleClick = () => setSidebarActive(false);
+
   return (
-    <div>SidebarMobile</div>
+    <div class={sidebarActive ? "sidebar-mobile active" : "sidebar-mobile"}>
+      <div className='header'>
+        <h1>
+            <Link className='logo' to="/">Devfolio</Link>
+        </h1>
+        <i class="bi bi-x-lg" onClick={handleClick}></i>
+      </div>
+      <ul>
+        <li>
+          <Link onClick={handleClick} to="/home">
+            <i className="bi bi-house-door-fill"></i>Home
+          </Link>
+        </li>
+        <li>
+          <Link onClick={handleClick} to="/about">
+            <i class="bi bi-person-lines-fill"></i>About me
+          </Link>
+        </li>
+        <li>
+          <Link onClick={handleClick} to="/work">
+            <i class="bi bi-code-slash"></i>My projects
+            </Link>
+        </li>
+        <li>
+          <Link onClick={handleClick} to="/contact">
+            <i class="bi bi-telephone-fill"></i>Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
