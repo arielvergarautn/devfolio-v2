2e911eaff1f47054a3ff17f24d17914dc5feaa5b
Ariel Vergara <avergara@intercept.cloud>
2022-10-03 07:33:22 +0200

    Adding transition css to buttons

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@
 import Banner from '../../components/single/Banner'
 //Animations
 import { motion } from 'framer-motion'
-import { pageAnimation } from '../../animation'
+import { fade, pageAnimation } from '../../animation'
 //Styles
 import './_home.scss'
 import { Link } from 'react-router-dom'
@@ -11,12 +11,12 @@ const Home = () => {
     return (
         <motion.div className='home' variants={pageAnimation} initial='hidden' animate='show'>
             <Banner />
-            <div className="sections">
+            <motion.div variants={fade} className="sections">
                 <Link to={"/about"}>About me</Link>
                 <Link to={"/myExperience"}>My experience</Link>
                 <Link to={"/work"}>My Projects</Link>
                 <Link to={"/contact"}>Contacts</Link>
-            </div>
+            </motion.div>
         </motion.div>
     )
 }
