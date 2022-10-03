ec84aa9f0e1f0068c4582ca4e3c3717d3a5dde22
Ariel Vergara <avergara@intercept.cloud>
2022-10-03 07:29:31 +0200

    New features

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,22 @@
 //Components
 import Banner from '../../components/single/Banner'
-import AboutMe from '../../components/single/aboutMe/AboutMe'
 //Animations
 import { motion } from 'framer-motion'
 import { pageAnimation } from '../../animation'
 //Styles
 import './_home.scss'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <motion.div className='home' variants={pageAnimation} initial='hidden' animate='show'>
             <Banner />
-            <AboutMe />
+            <div className="sections">
+                <Link to={"/about"}>About me</Link>
+                <Link to={"/myExperience"}>My experience</Link>
+                <Link to={"/work"}>My Projects</Link>
+                <Link to={"/contact"}>Contacts</Link>
+            </div>
         </motion.div>
     )
 }
